ed5262beb2fcb77e1b35210c9ffbf2cd3ea20b2a
Glenda <chicag9313@gmail.com>
2024-10-22 16:16:19 -0700

    refactor code

--- test.js
+++ test.js
@@ -1,39 +1,7 @@
-
 // The existing tests in this file should not be modified,
 // but you can add more tests if needed.
-
-const app = require('./src/app').app;
-const logger = require('./src/common/logger').logger;
-const repositoryClient = require('./src/storage/repository').repositoryClient;
 const supertest = require('supertest')
-
-let server; // http.Server
-
-/**
- * spin up a server for every test class.
- */
-beforeAll((done) => {
-    // disable noisy error logging
-    // use console.log directly if needed
-    jest.spyOn(logger, 'error').mockImplementation(() => { });
-
-    server = app.listen(done);
-});
-
-beforeEach((done) => {
-    // clears db to before each test case, to prevent impacting each others tests state
-    repositoryClient.clear()
-        .then(() => done());
-})
-
-/**
- * spin own the server after tests complete.
- */
-afterAll((done) => {
-    // after all test are executed, shutdown server
-    server.close(done);
-});
-
+const server = require('./server.js')
 describe('data-storage-api-node', () => {
     test('PUT', async () => {
         const putResult = await supertest(server)
@@ -42,7 +10,6 @@ describe('data-storage-api-node', () => {
             .set('Content-Type', 'text/plain')
             .set('Accept', 'application/json')
             .expect(201)
-
         expect(putResult.body).toBeTruthy()
         expect(typeof putResult.body).toBe('object')
         expect(putResult.body).toHaveProperty('oid')
@@ -51,18 +18,15 @@ describe('data-storage-api-node', () => {
         expect(putResult.body).toHaveProperty('size')
         expect(typeof putResult.body.size).toBe('number')
         expect(putResult.body.size).toEqual(9)
-
         const putResult2 = await supertest(server)
             .put('/data/my-repo')
             .send('other')
             .set('Content-Type', 'text/plain')
             .set('Accept', 'application/json')
             .expect(201)
-
         expect(putResult2.body.size).toEqual(5)
         expect(putResult.body.oid).not.toBe(putResult2.body.oid)
     })
-
     test('GET', async () => {
         const putResult1 = await supertest(server)
             .put('/data/my-repo')
@@ -70,22 +34,18 @@ describe('data-storage-api-node', () => {
             .set('Content-Type', 'text/plain')
             .set('Accept', 'application/json')
             .expect(201)
-
         await supertest(server)
             .get(`/data/my-repo/${putResult1.body.oid}`)
             .expect(200)
             .then(response => {
-                // TODO: determine why resonse.body is empty here
-                expect(response.text).toEqual('something')
+                expect(response.body).toEqual('something')
             })
     })
-
     test('GET not found', async () => {
         await supertest(server)
             .get(`/data/my-repo/missing`)
             .expect(404)
     })
-
     test('DELETE', async () => {
         const putResult = await supertest(server)
             .put('/data/my-repo')
@@ -93,35 +53,27 @@ describe('data-storage-api-node', () => {
             .set('Content-Type', 'text/plain')
             .set('Accept', 'application/json')
             .expect(201)
-
         const hash = putResult.body.oid
-
         const dupResult = await supertest(server)
             .put('/data/other-repo')
             .send('something')
             .set('Content-Type', 'text/plain')
             .set('Accept', 'application/json')
             .expect(201)
-
         const dupHash = dupResult.body.oid
-
         await supertest(server)
             .delete(`/data/my-repo/${hash}`)
             .expect(200)
-
         await supertest(server)
             .get(`/data/my-repo/${hash}`)
             .expect(404)
-
         await supertest(server)
             .get(`/data/other-repo/${dupHash}`)
             .expect(200)
             .then(response => {
-                // TODO: determine why resonse.body is empty here
-                expect(response.text).toEqual('something')
+                expect(response.body).toEqual('something')
             })
     })
-
     test('DELETE not found', async () => {
         await supertest(server)
             .delete('/data/my-repo/missing')
